5eda910d852ec22f8e4abcbf130f5867487e1a39
DOFER998 <killmy666@gmail.com>
2023-07-28 17:08:52 +0300

    Fix ceff

--- tg_bot/handlers/user/strategy_user.py
+++ tg_bot/handlers/user/strategy_user.py
@@ -39,7 +39,7 @@ async def losing(call: CallbackQuery):
     data = call.data.split(":")
     await call.message.answer(
         text=get_messages()['5'].format(user=call.message.chat.first_name, coeff=float(data[2]),
-                                        int=float(data[1]) * 1.5),
+                                        int=float(f'{float(data[1]) * 1.5:.2f}')),
         reply_markup=choice_user_stake(amount=float(data[1]) * 1.5, coeff=data[2]))
     await call.answer()
 
--- tg_bot/utils/random_coefficient.py
+++ tg_bot/utils/random_coefficient.py
@@ -3,4 +3,4 @@ import random
 
 def random_number():
     number = random.uniform(1.50, 2.00)
-    return number
+    return f'{number:.2f}'
