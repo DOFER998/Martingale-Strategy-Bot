199438ae8a4ee6bf1ccd43d358fa2283a5b7b3c0
DOFER998 <killmy666@gmail.com>
2023-08-02 21:10:23 +0300

    Fix

--- tg_bot/keyboards/admin/inline.py
+++ tg_bot/keyboards/admin/inline.py
@@ -13,7 +13,7 @@ def message_page(page: int = 1):
                                         [
                                             InlineKeyboardButton(text="⬅ Назад",
                                                                  callback_data=f"negative_message:{page - 1}"),
-                                            InlineKeyboardButton(text=f"| {page}/9 |",
+                                            InlineKeyboardButton(text=f"| {page}/11 |",
                                                                  callback_data="dont_click_me"),
                                             InlineKeyboardButton(text="Вперёд ➡",
                                                                  callback_data=f"plus_message:{page + 1}")
